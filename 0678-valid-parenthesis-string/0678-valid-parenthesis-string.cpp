0ac21e66f32fe39cf689ba4bc47945b3678ed1be
AmanCutinha <142223028+AmanCutinha@users.noreply.github.com>
2026-08-09 23:02:01 +0530

    Time: 0 ms (100%), Space: 8.1 MB (48.6%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int mn=0;
+        int mx=0;
+        for(char c:s){
+            if(c=='('){
+                mn++;
+                mx++;
+            }
+            else if(c==')'){
+                mn--;
+                mx--;
+            }
+            else{
+                mn--;
+                mx++;
+            }
+            if(mn<0) mn=0;
+            if(mx<0) return false;
+        }
+        return (mn==0);
+    }
+};
