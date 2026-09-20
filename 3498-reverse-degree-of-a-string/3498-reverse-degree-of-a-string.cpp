1abf8178b45963d30566306695dbd0a77607b58d
AmanCutinha <142223028+AmanCutinha@users.noreply.github.com>
2026-09-20 23:39:22 +0530

    Time: 0 ms (100%), Space: 9.9 MB (15.78%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int n=s.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            int c=s[i];
+            int h='z'-c+1;
+            sum+=(i+1)*h;
+        }
+        return sum;
+    }
+};
